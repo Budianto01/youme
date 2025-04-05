2d2f29eecea5241f2b3a90b6eee2c258a205ce3d
Upin_jawir <161274306+Budianto01@users.noreply.github.com>
2025-04-06 00:57:43 +0700

    youme.sh

--- youme.sh
+++ youme.sh
@@ -1,6 +1,4 @@
-#!/data/data/com.termux/files/usr/bin/bash
 
-# Cek dan install dependensi
 echo "Mengecek dependensi..."
 
 pkg install -y mpv python ffmpeg openssl curl
@@ -8,15 +6,14 @@ pkg install -y mpv python ffmpeg openssl curl
 pip install --upgrade pip
 pip install --upgrade yt-dlp certifi
 
-# Baca input link YouTube
 read -p "Masukkan link YouTube: " link
 
-# Cek apakah link kosong
+
 if [ -z "$link" ]; then
   echo "Link tidak boleh kosong."
   exit 1
 fi
 
-# Jalankan mpv dengan sertifikat yang valid
+
 echo "Memutar musik dari YouTube..."
 SSL_CERT_FILE=$(python -m certifi) mpv --no-video --ytdl-format bestaudio "$link"
