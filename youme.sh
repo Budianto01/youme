7b9b446cc316a3794caf05f6a6b6f4dcd6d1a720
Upin_jawir <161274306+Budianto01@users.noreply.github.com>
2025-04-06 01:07:44 +0700

    Update-youme.sh

--- youme.sh
+++ youme.sh
@@ -1,3 +1,4 @@
+
 echo "Mengecek dependensi..."
 
 pkg install -y mpv python ffmpeg openssl curl
@@ -6,8 +7,7 @@ pip install --upgrade pip
 pip install --upgrade yt-dlp certifi
 
 
-read -p "Masukkan link YouTube: " link
-
+read -p "Masukkan link playlist YouTube: " link
 
 if [ -z "$link" ]; then
   echo "Link tidak boleh kosong."
@@ -15,5 +15,20 @@ if [ -z "$link" ]; then
 fi
 
 
-echo "Memutar musik dari YouTube..."
-SSL_CERT_FILE=$(python -m certifi) mpv --no-video --ytdl-format=bestaudio "$link"
+tmpfile=$(mktemp)
+
+echo "Mengambil daftar video dari playlist..."
+yt-dlp -j --flat-playlist "$link" | jq -r '.url' | sed 's_^_https://www.youtube.com/watch?v=_' > "$tmpfile"
+
+
+shuf "$tmpfile" -o "$tmpfile"
+
+
+echo "Memutar playlist secara acak..."
+while IFS= read -r video; do
+  echo "Now playing: $video"
+  SSL_CERT_FILE=$(python -m certifi) mpv --no-video --ytdl-format=bestaudio "$video"
+done < "$tmpfile"
+
+
+rm "$tmpfile"
